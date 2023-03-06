b5d686fa6806252304419357e76433e5250dd3db
Lucas Katayama <lucas.katayama@allin.com.br>
2023-03-05 23:34:28 -0300

    refactor: update error name

--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,4 +1,4 @@
 export * from './unexpected-error'
 export * from './unprocessable-error'
 export * from './limit-request-error'
-export * from './not-found-error'
+export * from './not-found-user-error'
