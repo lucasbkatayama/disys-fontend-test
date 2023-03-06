5d2e3b1d26771c8ef10539f9b76bbdb6f6e3f754
Lucas Katayama <lucas.katayama@allin.com.br>
2023-03-05 23:01:33 -0300

    feat: add click prop to card component

--- src/presentation/components/card/card.tsx
+++ src/presentation/components/card/card.tsx
@@ -5,14 +5,15 @@ import './card.css'
 type PropsType = {
   avatar_url: string,
   login: string,
-  html_url: string
+  html_url: string,
+  onClick: () => void
 }
 
 const Card: React.FC<PropsType> = (props: PropsType) => {
-  const { avatar_url, login, html_url } = props
+  const { avatar_url, login, html_url, onClick } = props
 
   return (
-    <div className='card-container'>
+    <div className='card-container' onClick={onClick}>
       <div className='card'>
         <img loading='lazy' width={80} src={avatar_url} alt='profile' />
         <div className='card-text'>
