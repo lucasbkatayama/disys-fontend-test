3e7fe0a9b22aef51c3f987dd7338e265d182c014
Lucas Katayama <lucas.katayama@allin.com.br>
2023-03-06 10:56:34 -0300

    test: ensure it render search users and user data

--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,5 +1,23 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Github Search User App', () => {
+  beforeEach(() => {
+    cy.visit('/', { timeout: 2000 })
+  })
+
+  it('Search a list of users', () => {
+    cy.intercept('GET', 'https://api.github.com/search/*', { fixture: 'mock-users.json' })
+    const searchBar = cy.get('.search')
+    cy.get(".search").find('input').type('hello')
+    searchBar.find('button').click()
+    cy.get('.cards-wrap').children().should('have.length', 24)
+  })
+
+  it('Search a user and open its information', () => {
+    cy.intercept('GET', 'https://api.github.com/search/*', { fixture: 'mock-users.json' })
+    cy.intercept('GET', 'https://api.github.com/users/*', { fixture: 'mock-user.json' })
+    const searchBar = cy.get('.search')
+    cy.get(".search").find('input').type('hello')
+    searchBar.find('button').click()
+    cy.get('.cards-wrap').first().click()
+    cy.get('.user-card').should('exist')
   })
 })
